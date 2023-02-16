c4458efb3d23cfcd81e8fda2e5fc6b3b7d64e2cd
Rocio2795 <robustamante684@gmail.com>
2023-02-16 00:20:58 -0300

    modificacion en app.js y package.json

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const express = require ("express");
 const path= require ("path");
-
+const port = process.env.PORT || 3002;
 const app = express(); 
 
 const publicFolderPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicFolderPath));
 
-const { listen } = require("express/lib/application");
+/*const { listen } = require("express/lib/application");*/
 
 app.get("/", (req, res) =>{
-    res.sendFile(path.resolve (__dirname, "./views/home.html"))
+    res.sendFile(path.join(__dirname, "./views/home.html"))
 });
 
+/*app.listen(3002, ()=>console.log("servidor corriendo en el puerto 3002"));*/
 
-app.listen(3002, ()=>console.log("servidor corriendo en el puerto 3002"));
-
-
+app.listen(port, () => console.log(`Server running on port ${port}`));
